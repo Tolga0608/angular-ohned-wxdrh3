e64b3a7ffa363ec797e039418658018b8eb70e22
Tolga0608 <79638365+Tolga0608@users.noreply.github.com>
2023-02-09 12:36:03 +0100

    2 files modified

--- src/app/services/to-do.service.ts
+++ src/app/services/to-do.service.ts
@@ -11,7 +11,7 @@ export class ToDoDataService {
 
   constructor() {
     this.toDos.push(new ToDo('clean my bedroom', true, '09.03.2022'));
-    this.toDos.push(new ToDo("learning english with Emre ", false,"No Frist"));
+    this.toDos.push(new ToDo("learning english with Emre & Enes ", false,"No Frist"));
     this.toDos.push(new ToDo("Sleep", false,"No Frist"));
     this.toDos.push(new ToDo("Fight against Emre Remzi", false, "Friday Night at 6am"));
     this.toDos.push(new ToDo("playing football",true, "31.12.2023"))
